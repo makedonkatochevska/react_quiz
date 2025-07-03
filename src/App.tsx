63c9866d905ac81024036174a4a9620cc2e7b8d2
Makedonka Tochevska <makedonkatochevska@gmail.com>
2025-07-03 09:45:42 +0200

    routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import QuizPage from "./pages/QuizPage";
+import { ROUTES } from "./routes/Routes";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.quizPage} element={<QuizPage />}></Route>
+          <Route path={ROUTES.errorPage} element={<ErrorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
       <QuizPage />
     </>
   );
